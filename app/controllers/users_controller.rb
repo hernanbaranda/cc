f38e715614721edbb7264a2bf1b4cff0b5037747
hernanbaranda <hernanbaranda@gmail.com>
2023-05-18 11:15:04 -0300

    Initialize repository

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    # Not the final implementation!
+    if @user.save
+      reset_session
+      log_in @user
+      flash[:success] = "Welcome to the CC App!"
+      redirect_to @user
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+end
