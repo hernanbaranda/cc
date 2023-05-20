26f0fdd929e73b3118bc6c359877a3d8cefae7a8
hernanbaranda <hernanbaranda@gmail.com>
2023-05-20 18:30:36 -0300

    Implement advanced login 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -16,7 +17,7 @@ class UsersController < ApplicationController
     if @user.save
       reset_session
       log_in @user
-      flash[:success] = "Welcome to the CC App!"
+      flash[:success] = "Bienvenido a la app CC!"
       redirect_to @user
     else
       render 'new', status: :unprocessable_entity
@@ -30,7 +31,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:success] = "Profile updated"
+      flash[:success] = "Perfil actualizado"
       redirect_to @user
     else
       render 'edit', status: :unprocessable_entity
@@ -47,8 +48,14 @@ class UsersController < ApplicationController
     # Confirms a logged-in user.
     def logged_in_user
       unless logged_in?
-        flash[:danger] = "Please log in."
+        flash[:danger] = "Por favor logueese"
         redirect_to login_url, status: :see_other
       end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url, status: :see_other) unless @user == current_user
     end
 end
